6c30d07f286a013e53b3e7c83126c1558321a06b
Alt13 <Alt90@rambler.ru>
2017-01-08 17:06:44 +0300

    Добавил генератор папок проектов

--- site_generator.py
+++ site_generator.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+import os
 
 
 def get_args():
@@ -11,5 +13,24 @@ def get_args():
     return parser.parse_args()
 
 
+def create_index_html(site_directory, pages_list):
+    os.mkdir(site_directory)
+
+
+def create_page_html(name_of_site, site_directory, page):
+    pass
+
+
+def create_site(name_of_site, pages_list):
+    site_directory = '%s_html' % name_of_site
+    create_index_html(site_directory, pages_list)
+    for page in pages_list:
+        create_page_html(name_of_site, site_directory, page)
+
+
 if __name__ == '__main__':
     args = get_args()
+    config = json.load(args.config)
+    name_of_sites = list(config.keys() - ['topics'])
+    for name in name_of_sites:
+        create_site(name, config[name])
