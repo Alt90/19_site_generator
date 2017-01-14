86654ce2d1fe104202dc6d4f9085f10ef9f82d38
Alt13 <Alt90@rambler.ru>
2017-01-14 16:00:38 +0300

    Поправил названия Заголовков

--- site_generator.py
+++ site_generator.py
@@ -27,29 +27,33 @@ def get_page_html_info(page):
     return {'title': page['title'],
             'source': get_source_html_path(page['source'])}
 
+def revert_topics_list_to_dict(topics_list):
+    return {topic['slug']:topic['title'] for topic in topics_list}
 
-def get_site_structure(pages_list):
+
+def get_site_structure(pages_list, topics):
     site_structure = {}
     for page in pages_list:
-        site_structure_list = site_structure.get(page['topic'], [])
+        site_structure_list = site_structure.get(topics[page['topic']], [])
         site_structure_list.append(get_page_html_info(page))
-        site_structure[page['topic']] = site_structure_list
+        site_structure[topics[page['topic']]] = site_structure_list
     return site_structure
 
 
-def get_index_html(pages_list):
+def get_index_html(pages_list, topics_list):
     env = Environment(loader=FileSystemLoader('templates'))
     template = env.get_template('index.html')
-    site_structure = get_site_structure(pages_list)
+    topics = revert_topics_list_to_dict(topics_list)
+    site_structure = get_site_structure(pages_list, topics)
     return template.render(topics=site_structure.keys(), pages=site_structure)
 
 
-def create_index_html_file(site_directory, pages_list):
+def create_index_html_file(site_directory, pages_list, topics_list):
     if not os.path.exists(site_directory):
         os.mkdir(site_directory)
     page_index_path = os.path.join(site_directory, 'index.html')
     with open(page_index_path, "w") as index_file:
-        index_file.write(get_index_html(pages_list))
+        index_file.write(get_index_html(pages_list, topics_list))
 
 
 def get_page_content(page_source_path):
@@ -70,9 +74,9 @@ def create_page_html_file(name_of_site, site_directory, page):
         page_html_file.write(get_page_html(page_source_path))
 
 
-def create_site(name_of_site, pages_list):
+def create_site(name_of_site, pages_list, topics_list):
     site_directory = '%s_html' % name_of_site
-    create_index_html_file(site_directory, pages_list)
+    create_index_html_file(site_directory, pages_list, topics_list)
     for page in pages_list:
         create_page_html_file(name_of_site, site_directory, page)
 
@@ -82,4 +86,4 @@ if __name__ == '__main__':
     config = json.load(args.config)
     name_of_sites = list(config.keys() - ['topics'])
     for name in name_of_sites:
-        create_site(name, config[name])
+        create_site(name, config[name], config['topics'])
